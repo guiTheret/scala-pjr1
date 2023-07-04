60f64e758d58334bc2438c8ce7302ba90732daa0
PabloEfrei <74155592+PabloEfrei@users.noreply.github.com>
2023-07-04 11:10:05 +0200

    Update Main.scala
    Add imports

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,11 @@
+import zio._
+import zio.Console._
+import zio.nio._
+import zio.nio.channels._
+import zio.nio.file.Path
+import java.io.IOException
+import zio.json._
+
 @main def hello: Unit =
   println("Hello world!")
   println(msg)
