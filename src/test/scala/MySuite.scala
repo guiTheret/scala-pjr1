b940b6c5c268a532a69d37964ca10be60981a7e7
PabloEfrei <74155592+PabloEfrei@users.noreply.github.com>
2023-07-04 11:10:52 +0200

    Update MySuite.scala
    Add imports and writeAndThenRead

--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -1,5 +1,14 @@
 // For more information on writing tests, see
 // https://scalameta.org/munit/docs/getting-started.html
+
+import zio._
+import zio.Console._
+import zio.nio._
+import zio.nio.channels._
+import zio.nio.file.Path
+import java.io.IOException
+import zio.json._
+
 class MySuite extends munit.FunSuite {
   test("example test that succeeds") {
     val obtained = 42
@@ -7,3 +16,6 @@ class MySuite extends munit.FunSuite {
     assertEquals(obtained, expected)
   }
 }
+
+private def writeAndThenRead(file: Path)(bytes: Chunk[Byte]) =
+  Files.writeBytes(file, bytes) *> Files.readAllBytes(file)
