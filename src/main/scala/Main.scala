7ae50fbf32328faed4241c2073ed0f0204c39d5b
guiTheret <gtheret@sfr.fr>
2023-07-10 16:40:59 +0200

    fix merge branch

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,46 +1,44 @@
 import zio._
-import zio.Console._
-import zio.nio._
-import zio.nio.channels._
-import zio.nio.file.{Path, Files}
-import java.io.IOException
 import zio.json._
-import zio.stream.ZStream
-import scala.util.boundary, boundary.break
+
+import java.io.FileNotFoundException
+import scala.io.Source
+import scala.util.{Failure, Success, Try}
+
 
 type SudokuGrid = Array[Array[Int]]
 
 def solveSudoku(grid: SudokuGrid): Option[SudokuGrid] = {
   //Recursive backtracking function
-  def solve(row: Int, col: Int): Option[SudokuGrid] = boundary {
-  // if all cells have been filled, then the solution is found
-  if (row == 9) {
-    break(Some(grid))
-  // if we reached the end of a row, we move to the next row
-  } else if (col == 9) {
-    solve(row + 1, 0)
-  // if a cell is already filled, we move to the next cell
-  } else if (grid(row)(col) != 0) {
-    solve(row, col + 1)
-  // trying values from 1 to 9 for the last cell of value 0 value
-  } else {
-    // try all possible values for the current cell
-    for (num <- 1 to 9) {
-      // isValid to check that the current value is the correct one
-      if (isValid(row, col, num)) {
-        grid(row)(col) = num
-        solve(row, col + 1) match {
-          // if a solution has been been, it is returned
-          case Some(solution) => break(Some(solution))
-          // if no solution is found, the cell is reset to 0
-          case None => grid(row)(col) = 0
+  def solve(row: Int, col: Int): Option[SudokuGrid] = {
+    // if all cells have been filled, then the solution is found
+    if (row == 9) {
+      Some(grid)
+    // if we reached the end of a row, we move to the next row
+    } else if (col == 9) {
+      solve(row + 1, 0)
+    // if a cell is already filled, we move to the next cell
+    } else if (grid(row)(col) != 0) {
+      solve(row, col + 1)
+    // trying values from 1 to 9 for the last cell of value 0 value
+    } else {
+      // try all possible values for the current cell
+      for (num <- 1 to 9) {
+        // isValid to check that the current value is the correct one
+        if (isValid(row, col, num)) {
+          grid(row)(col) = num
+          solve(row, col + 1) match {
+            // if a solution has been been, it is returned
+            case Some(solution) => return Some(solution)
+            // if no solution is found, the cell is reset to 0
+            case None => grid(row)(col) = 0
+          }
         }
       }
+      // if no solution is found, "None" is returned, to try a different value for the previous cell
+      None
     }
-    // if no solution is found, "None" is returned, to try a different value for the previous cell
-    break(None)
   }
-}
 
 
 
@@ -69,7 +67,7 @@ def solveSudoku(grid: SudokuGrid): Option[SudokuGrid] = {
   solve(0, 0)
 }
 
-def printGrid(sudoku: SudokuGrid): Unit = {
+def printGrid(sudoku: SudokuGrid): String = {
   val separator = "+------+-------+------+"
   val formattedRows = sudoku.grouped(3).map { bigGroup =>
     bigGroup.map { row =>
@@ -78,34 +76,46 @@ def printGrid(sudoku: SudokuGrid): Unit = {
       }.mkString(" | ")
     }.mkString("\n")
   }.mkString("\n\n")
+  formattedRows
+  //println(formattedRows)
+}
 
-  println(formattedRows)
+
+def parseFile(filePath: String) = {
+  val file = Try(Source.fromFile("src/ressources/" + filePath))
+  file match {
+    case Success(value) => ZIO.succeed(value.mkString)
+    case Failure(exception) => ZIO.fail(exception)
+  }
+}
+
+def buildGrid(jsonString: String) = {
+  val sudoku = jsonString.fromJson[SudokuGrid]
+  sudoku match {
+    case Right(value) => ZIO.succeed(value)
+    case Left(exception) => ZIO.fail(exception)
+  }
 }
 
 
 
+object Main extends ZIOAppDefault {
+  def run =
+    for {
+      _ <- Console.printLine("Enter the path to the JSON file containing the Sudoku problem:")
+      path <- Console.readLine
+      _ <-  Console.printLine(s"You entered: $path")
 
+      jsonString <- parseFile(path)
 
-@main def hello: Unit =
-  println("Hello world!")
-  println(msg)
-  val puzzle: SudokuGrid = Array(
-    Array(5, 3, 0, 0, 7, 0, 0, 0, 0),
-    Array(6, 0, 0, 1, 9, 5, 0, 0, 0),
-    Array(0, 9, 8, 0, 0, 0, 0, 6, 0),
-    Array(8, 0, 0, 0, 6, 0, 0, 0, 3),
-    Array(4, 0, 0, 8, 0, 3, 0, 0, 1),
-    Array(7, 0, 0, 0, 2, 0, 0, 0, 6),
-    Array(0, 6, 0, 0, 0, 0, 2, 8, 0),
-    Array(0, 0, 0, 4, 1, 9, 0, 0, 5),
-    Array(0, 0, 0, 0, 8, 0, 0, 7, 9)
-  )
-  solveSudoku(puzzle) match {
-    case Some(solution) =>
-      println("Solution:")
-      printGrid(solution)
-    case None =>
-      println("No solution found.")
-  }
+      _ <- Console.printLine(s"JSON String: $jsonString")
+    
+      grid <- buildGrid(jsonString)
+      _ <- Console.printLine(printGrid(grid))
 
-def msg = "I was compiled by Scala 3. :)"
+      _ <- solveSudoku(grid) match {
+        case Some(value) => Console.printLine(printGrid(value))
+        case None => Console.printLine("No solution found")
+      }
+    } yield ()
+}
