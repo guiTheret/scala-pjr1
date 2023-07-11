d98a3af4f16873541b97aed1de4aac3ec18afa42
Theret Guillaume <73123940+guiTheret@users.noreply.github.com>
2023-07-11 10:26:06 +0100

    Squashed commit of the following:
    commit f4135127c989d04b5c2f2de894119044dffa6ccc
    Author: Theret Guillaume <73123940+guiTheret@users.noreply.github.com>
    Date:   Tue Jul 11 10:25:56 2023 +0100
    
        up
    
    commit baf378f6ae6c329ee7eb8f44bde7a7cbd3014b66
    Author: Theret Guillaume <73123940+guiTheret@users.noreply.github.com>
    Date:   Tue Jul 11 10:25:48 2023 +0100
    
        feat: add 2 tests
    
    commit e03123c3b3a305b4a2cf49230c2b75cba2c2bfd8
    Author: Theret Guillaume <73123940+guiTheret@users.noreply.github.com>
    Date:   Mon Jul 10 22:19:34 2023 +0100
    
        up

--- build.sbt
+++ build.sbt
@@ -13,6 +13,11 @@ lazy val root = project
       "org.scalameta" %% "munit" % "0.7.29" % Test,
       "dev.zio" %% "zio" % "2.0.15",
       "dev.zio" %% "zio-nio" % "2.0.1",
-      "dev.zio" %% "zio-json" % "0.6.0"
+      "dev.zio" %% "zio-json" % "0.6.0",
+      "dev.zio" %% "zio-test"          % "2.0.15" % Test,
+      "dev.zio" %% "zio-test-sbt"      % "2.0.15" % Test,
+      "dev.zio" %% "zio-test-magnolia" % "2.0.15" % Test
+      // add test libraries here
+
     )
   )
--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -1,10 +1,74 @@
-// For more information on writing tests, see
-// https://scalameta.org/munit/docs/getting-started.html
+import zio.test._
+import zio.test.Assertion._
+import zio.Console._
 
-class MySuite extends munit.FunSuite {
-  test("example test that succeeds") {
-    val obtained = 42
-    val expected = 42
-    assertEquals(obtained, expected)
+import scala.util.control.Breaks._
+
+object SudokuSolverSpec extends ZIOSpecDefault  {
+  def spec = suite("Sudoku Solver")(
+      test("solveSudoku should solve a valid Sudoku puzzle") {
+        val puzzle = Array(
+          Array(5, 3, 0, 0, 7, 0, 0, 0, 0),
+          Array(6, 0, 0, 1, 9, 5, 0, 0, 0),
+          Array(0, 9, 8, 0, 0, 0, 0, 6, 0),
+          Array(8, 0, 0, 0, 6, 0, 0, 0, 3),
+          Array(4, 0, 0, 8, 0, 3, 0, 0, 1),
+          Array(7, 0, 0, 0, 2, 0, 0, 0, 6),
+          Array(0, 6, 0, 0, 0, 0, 2, 8, 0),
+          Array(0, 0, 0, 4, 1, 9, 0, 0, 5),
+          Array(0, 0, 0, 0, 8, 0, 0, 7, 9)
+        )
+
+        val expectedSolution = Array(
+          Array(5, 3, 4, 6, 7, 8, 9, 1, 2),
+          Array(6, 7, 2, 1, 9, 5, 3, 4, 8),
+          Array(1, 9, 8, 3, 4, 2, 5, 6, 7),
+          Array(8, 5, 9, 7, 6, 1, 4, 2, 3),
+          Array(4, 2, 6, 8, 5, 3, 7, 9, 1),
+          Array(7, 1, 3, 9, 2, 4, 8, 5, 6),
+          Array(9, 6, 1, 5, 3, 7, 2, 8, 4),
+          Array(2, 8, 7, 4, 1, 9, 6, 3, 5),
+          Array(3, 4, 5, 2, 8, 6, 1, 7, 9)
+        )
+
+        val result = solveSudoku(puzzle)
+        assert(areArraysEqual(result.get, expectedSolution))(isTrue)
+      },
+      test("solveSudoku should fail") {
+        val puzzle = Array(
+          Array(5, 3, 0, 0, 7, 0, 0, 0, 0),
+          Array(6, 0, 0, 1, 9, 5, 0, 0, 0),
+          Array(0, 9, 8, 0, 0, 0, 0, 6, 0),
+          Array(8, 0, 0, 0, 6, 0, 0, 0, 3),
+          Array(4, 0, 0, 8, 0, 3, 0, 0, 1),
+          Array(7, 0, 0, 0, 2, 0, 0, 0, 6),
+          Array(0, 6, 0, 0, 0, 0, 2, 8, 0),
+          Array(0, 0, 0, 4, 1, 9, 0, 0, 5),
+          Array(6, 0, 0, 0, 8, 0, 0, 7, 9)
+        )
+
+        val result = solveSudoku(puzzle)
+        assert(result)(isNone)
+      }
+    )
+}
+
+def areArraysEqual(arr1: Array[Array[Int]], arr2: Array[Array[Int]]): Boolean = {
+  if (arr1.length != arr2.length || arr1(0).length != arr2(0).length)
+    return false
+
+  var equal = true
+
+  breakable {
+    for (i <- 0 until arr1.length) {
+      for (j <- 0 until arr1(0).length) {
+        if (arr1(i)(j) != arr2(i)(j)) {
+          equal = false
+          break
+        }
+      }
+    }
   }
+
+  equal
 }
